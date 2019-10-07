0cce9f4420b112fe88fe17d863f1bfcca857b1ad
choumosg <g.choumos@gmail.com>
2019-10-08 01:58:13 +0300

    Update URLs - Refactor views and use shortcuts for rendering and 404 - Add results page placeholder - Add templates - Data update

--- conferenceLoader/views.py
+++ conferenceLoader/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Team
-from .models import Person
+from django.shortcuts import render, get_object_or_404
+from .models import Team, Person
 
-# Create your views here.
+# This is if the detectGuru part of the url is removed. Consider redirecting to a meaningful page.
 def index(request):
     return HttpResponse("Hello, world. You're at the conference index.")
 
+
 def detect_guru(request, project):
-    try:
-        team_name = Team.objects.get(project_code=project).name
-        team_pk = Team.objects.get(project_code=project).pk
-        team_people = Person.objects.all().filter(team=team_pk)
-        output = "Guru Detector for {0}.\n".format(team_name)
-        output += "Here are the people of the team:\n"
-        output += '\n'.join(x.name for x in team_people)
-        return HttpResponse(output)
-        # return HttpResponse("Guru Detector for {0}".format(team_name))
-    except Team.DoesNotExist:
-        return HttpResponse("The team corresponding to {0} does not exist. Did you manually modify the URL?".format(project))
+    team = get_object_or_404(Team, project_code=project)
+    context = {
+        'team_name': team.name,
+        'team_people': Person.objects.all().filter(team=team.pk),
+    }
+    return render(request, 'conferenceLoader/team_people.html', context)
+
 
 # This is the case where a team has not been specified. Later on it should be updated so that
 # the user will have the ability to change a Team from a list.
 def detect_guru_no_spec(request):
     return HttpResponse("A Team has not been specified. Did you manually modify the URL?")
+
+
+# The response page
+def results(request, project):
+    return HttpResponse("{0} - This is the Guru Results page.".format(project))
